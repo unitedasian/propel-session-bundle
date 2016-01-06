418be91ae38438d1be2243f52d823f0bc30d3127
Olivier Pichon <op@united-asian.com>
2016-01-06 19:07:25 +0700

    renamed extension

--- DependencyInjection/UAMPropelSessionExtension.php
+++ DependencyInjection/UAMPropelSessionExtension.php
@@ -7,7 +7,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class PropelSessionExtension extends Extension
+class UAMPropelSessionExtension extends Extension
 {
     /**
      * {@inheritDoc}
